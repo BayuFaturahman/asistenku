983a11d1e27997fc0aab6ffa53c1bdd09e19d45d
Bayu Faturahman <bayu.faturahman8@gmail.com>
2021-11-23 15:40:05 +0700

    Initial commit

---
+++ lib/nonLogged/onboardingSplash/page_onboarding.dart
@@ -0,0 +1,41 @@
+import 'package:asistenku/nonLogged/onboardingSplash/page_start_app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_screen/OnbordingData.dart';
+import 'package:flutter_onboarding_screen/flutteronboardingscreens.dart';
+
+class PageOnboarding extends StatelessWidget {
+  final List<OnbordingData> list = [
+    OnbordingData(
+      imagePath: "assets/ilustrasi/ilustrasi_1.png",
+      title: "Cleaner",
+      desc:
+          "Cleaner is one of the features of Assistenku This feature provides a full house cleaning service, to find out more about the features of Assistenku , please continue",
+    ),
+    OnbordingData(
+      imagePath: "assets/ilustrasi/ilustrasi_2.png",
+      title: "Ironing",
+      desc:
+          "Iron is one of the features of the Assistant This feature provides services for ironing your clothes, to find out more about the features of Assistenku , please continue",
+    ),
+    OnbordingData(
+      imagePath: "assets/ilustrasi/ilustrasi_3.png",
+      title: "Washing",
+      desc:
+          "Washing is one of the features of the Assistant This feature provides laundry services, to find out more about the My Assistant feature, please continue",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: IntroScreen(
+          list,
+          MaterialPageRoute(builder: (context) => PageStartApp()),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/nonLogged/onboardingSplash/page_start_app.dart
@@ -0,0 +1,81 @@
+import 'package:asistenku/nonLogged/onboardingSplash/page_onboarding.dart';
+import 'package:asistenku/widget/button_primary.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class PageStartApp extends StatefulWidget {
+  @override
+  PageStartAppState createState() => PageStartAppState();
+}
+
+class PageStartAppState extends State<PageStartApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 100),
+        child: ButtonPrimary(
+          onPressed: () => Get.offAll(() => PageOnboarding()),
+          height: 60,
+          radius: 15.0,
+          label: 'Get Started !',
+          labelStyle:
+              const TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            //head
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Assistenku",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            //content
+            SizedBox(
+              height: Get.height / 2,
+              width: Get.width / 1.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/logo/logo.png',
+                    height: 100,
+                    width: 100,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Text(
+                    "Welcomes",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Asistenku. is a household assistant service provider. such as cleaning the house, washing clothes and so on",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
